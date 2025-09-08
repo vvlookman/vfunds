435c348ced430017db08b850cd5d1b9edbbad9e3
vvlookman <vvlookman@gmail.com>
2025-09-08 17:57:06 +0800

    Use same date formats in qmt_http.py

--- external/qmt_http.py
+++ external/qmt_http.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sys
 import uvicorn
 import xtquant
+from datetime import datetime
 from fastapi import FastAPI, HTTPException
 from xtquant import xtdata
 
@@ -17,18 +18,22 @@ def df_to_json(df):
 @app.get("/dividend/{stock}")
 def dividend(stock: str):
     df = xtdata.get_divid_factors(stock_code=stock)
+    df['date'] = pd.to_datetime(df['time'], unit='ms').dt.tz_localize(
+        'UTC').dt.tz_convert('Asia/Shanghai').dt.strftime('%Y-%m-%d')
 
     return df_to_json(df)
 
 
 @app.get("/kline/{stock}")
 def kline(stock: str, period: str = '1d', dividend_type: str = 'front_ratio'):
-    xtdata.download_history_data2(stock, period, incrementally=True)
+    xtdata.download_history_data2(stock, period)
     data = xtdata.get_market_data(
         stock_list=[stock], dividend_type=dividend_type)
 
     df = pd.concat([df.T for df in data.values()], axis=1)
     df.columns = data.keys()
+    df['date'] = pd.to_datetime(df['time'], unit='ms').dt.tz_localize(
+        'UTC').dt.tz_convert('Asia/Shanghai').dt.strftime('%Y-%m-%d')
 
     return df_to_json(df)
 
@@ -39,6 +44,8 @@ def report(stock: str, table: str = 'PershareIndex'):
     data = xtdata.get_financial_data(stock_list=[stock], table_list=[table])
 
     df = data[stock][table]
+    df['date'] = df['m_anntime'].str.replace(
+        r'(\d{4})(\d{2})(\d{2})', r'\1-\2-\3', regex=True)
 
     return df_to_json(df)
 
